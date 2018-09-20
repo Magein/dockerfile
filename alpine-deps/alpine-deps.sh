fbcfb51f1446065610edf7be23b606e40cdd732f
Magein <16146901+ml178@users.noreply.github.com>
2018-09-20 11:09:14 +0800

    update

--- alpine-deps/alpine-deps.sh
+++ alpine-deps/alpine-deps.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
-
-usage(){
+Usage(){
     echo "Usage:"
     echo "  manager dependent, create or delete,default create"
     echo "  alpine-deps [options]"
-    echo "  alpine-deps -d"
+    echo "  alpine-deps -d base"
+    echo "  alpine-deps -d build"
+    echo "  alpine-deps -d base -d build"
     echo "Options:"
     echo "  -d,--delete             delete dependents"
     echo "  -h,--help               show information for help"
 }
 
-options=$(getopt -o dh --long delete,help -n "error" -- "$@")
+options=$(getopt -o d:h --long delete:,help -n "error" -- "$@")
 
 if [ $? -ne 0 ]; then
    exit 0
@@ -18,17 +19,18 @@ fi
 
 eval set -- "${options}"
 
-delete=0
+delete=()
 
 while true;
 do
     case $1 in
         -d|--delete)
-            delete=1
-            shift 1
+            length="${#delete}"
+            delete[$length]="."$2"-deps"
+            shift 2
         ;;
         -h|--help)
-            usage
+            Usage
             exit 0
         ;;
         *)
@@ -37,19 +39,25 @@ do
     esac
 done
 
-
-if [ "$delete" -eq 1 ]; then
-    apk del .build-deps
+if [ "$delete" ]; then
+    for i in "${delete[@]}";
+    do
+        apk del "$i"
+    done
 else
+    # 安装依赖需要的基础的文件，这里确认不用的时候，可调用删除
     apk add --no-cache --virtual .build-deps \
         autoconf \
+        g++ \
+        gcc \
+        make
+
+    # 效验压缩包、下载、编译用到的工具，服务安装完成后即可删除
+    apk add --no-cache --virtual .base-deps \
         dpkg-dev \
         dpkg \
         file \
-        g++ \
-        gcc \
         libc-dev \
-        make \
         pkgconf \
         re2c \
         gnupg \
